c4a1f770bbaec464b344eb2d60c79d80b2289767
Josh Gibbs <upaymeifixit@gmail.com>
2018-02-21 17:38:13 -0800

    project2: Added ability to export output to document

--- project2/src/NQueen.java
+++ project2/src/NQueen.java
@@ -1,36 +1,41 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class NQueen {
 
-  private static final boolean DEBUG = false;
+  public static void main (String[] args) throws FileNotFoundException {
+    final int[] size = askSizeRange();
+    final int trials = askTrials();
+    final double mutation_probability = askProbability();
+    final int population_size = askPopulationSize();
+    final String output_file_name = askFileName();
 
-  public static void main (String[] args) {
-    if (DEBUG) {
-      printStats(8, 8, 5, 25, 0.5);
-    } else {
-      final int[] size = askSizeRange();
-      final int trials = askTrials();
-      final double mutation_probability = askProbability();
-      final int population_size = askPopulationSize();
-
-      printStats(size[0], size[1], trials, population_size, mutation_probability);
-    }
+    String output = printStats(size[0], size[1], trials, population_size, mutation_probability);
+    System.out.print(output);
+    if (output_file_name != null) {
+      outputToFile(output_file_name, output);
+    } 
   }
 
-  private static void printSolutions (int smallest_n, Node[][] genetic, Node[][] rrhc) {
-    System.out.println("┌────┬───────────────────────────────────┬───────────────────────────────────┐");
-    System.out.println("│ N  │   Random Restart Hill Climbing    │         Genetic Algorithm         │");
-    System.out.println("├────┼───────────────────────────────────┼───────────────────────────────────┤");
+  private static void outputToFile (String file_name, String output) throws FileNotFoundException {
+    PrintWriter print_writer = new PrintWriter(file_name);
+    print_writer.print(output);
+    print_writer.close();
+  }
+
+  private static String printSolutions (int smallest_n, Node[][] genetic, Node[][] rrhc) {
+    StringBuilder string_builder = new StringBuilder();
+    string_builder.append("┌────┬───────────────────────────────────┬───────────────────────────────────┐\n");
+    string_builder.append("│ N  │   Random Restart Hill Climbing    │         Genetic Algorithm         │\n");
+    string_builder.append("├────┼───────────────────────────────────┼───────────────────────────────────┤\n");
     for (int i = 0; i < genetic.length; i++) {
       int n = i + smallest_n;
 
       String left_space = leftPad((int)(35 - 2 * n) / 2, ' ');
       String right_space = leftPad((int)(35 - 2 * n + 1) / 2, ' ');
-      System.out.println("│    │                                   │                                   │");
+      string_builder.append("│    │                                   │                                   │\n");
       // Print first three solutions side by side
       for (int j = 0; j < genetic[i].length; j++) {
         if (genetic[i][j] == null) {
@@ -41,22 +46,23 @@ public class NQueen {
         for (int l = 0; l < n; l++) {
           // Print left bar + N
           if (j == 0 && l == 1) {
-            System.out.printf("│ %2d │", n);
+            string_builder.append(String.format("│ %2d │", n));
           } else {
-            System.out.print("│    │");
+            string_builder.append("│    │");
           }
-          System.out.print(left_space + rrhc_lines[l] + right_space + '│' + left_space + gen_lines[l] + right_space + "│\n");
+          string_builder.append(left_space + rrhc_lines[l] + right_space + '│' + left_space + gen_lines[l] + right_space + "│\n");
         }
-        System.out.println("│    │                                   │                                   │");
+        string_builder.append("│    │                                   │                                   │\n");
       }
 
       if (i < genetic.length - 1) {
-        System.out.println("├────┼───────────────────────────────────┼───────────────────────────────────┤");
+        string_builder.append("├────┼───────────────────────────────────┼───────────────────────────────────┤\n");
       } else {
-        System.out.println("└────┴───────────────────────────────────┴───────────────────────────────────┘");
+        string_builder.append("└────┴───────────────────────────────────┴───────────────────────────────────┘\n");
       }
 
     }
+    return string_builder.toString();
   }
 
   private static String leftPad (int n, char c) {
@@ -67,29 +73,33 @@ public class NQueen {
     return string_builder.toString();
   }
 
-  private static void printStats (int min, int max, int trials, int population_size, double mutation_probability) {
+  private static String printStats (int min, int max, int trials, int population_size, double mutation_probability) {
     Stats gen_stats = getGeneticSolution(min, max, trials, population_size, mutation_probability);
     Stats rrhc_stats = getRRHCSolution(min, max, trials);
 
-    printSolutions(min, gen_stats.solutions, rrhc_stats.solutions);
+    StringBuilder string_builder = new StringBuilder();
+    String solutions = printSolutions(min, gen_stats.solutions, rrhc_stats.solutions);
+    string_builder.append(solutions);
 
-    System.out.println("┌────┬─────────────────────┬─────────────────┬─────────────────┬─────────────┐");
-    System.out.println("│    │   Nodes Generated   │    Run Time     │ Restarts / Gens │  % Solved   │");
-    System.out.println("│ N  ├──────────┬──────────┼────────┬────────┼────────┬────────┼──────┬──────┤");
-    System.out.println("│    │   RRHC   │    Gen   │  RRHC  │  Gen   │  RRHC  │  Gen   │ RRHC │ Gen  │");
-    System.out.println("├────┼──────────┼──────────┼────────┼────────┼────────┼────────┼──────┼──────┤");
+    string_builder.append("┌────┬─────────────────────┬─────────────────┬─────────────────┬─────────────┐\n");
+    string_builder.append("│    │   Nodes Generated   │    Run Time     │ Restarts / Gens │  % Solved   │\n");
+    string_builder.append("│ N  ├──────────┬──────────┼────────┬────────┼────────┬────────┼──────┬──────┤\n");
+    string_builder.append("│    │   RRHC   │    Gen   │  RRHC  │  Gen   │  RRHC  │  Gen   │ RRHC │ Gen  │\n");
+    string_builder.append("├────┼──────────┼──────────┼────────┼────────┼────────┼────────┼──────┼──────┤\n");
     for (int d = 0; d < max - min + 1; d++) {
-      System.out.printf("│ %2d ", d + min);
-      System.out.printf("│ %8.0f ", rrhc_stats.average_nodes_created[d]);
-      System.out.printf("│ %8.0f ", gen_stats.average_nodes_created[d]);
-      System.out.printf("│ %4dms ", rrhc_stats.average_cpu_time[d]);
-      System.out.printf("│ %4dms ", gen_stats.average_cpu_time[d]);
-      System.out.printf("│ %6.0f ", rrhc_stats.average_restarts[d]);
-      System.out.printf("│ %6.0f ", gen_stats.average_generations[d]);
-      System.out.printf("│ %3.0f%% ", rrhc_stats.percent_solved[d] * 100);
-      System.out.printf("│ %3.0f%% │\n", gen_stats.percent_solved[d] * 100);
+      string_builder.append(String.format("│ %2d ", d + min));
+      string_builder.append(String.format("│ %8.0f ", rrhc_stats.average_nodes_created[d]));
+      string_builder.append(String.format("│ %8.0f ", gen_stats.average_nodes_created[d]));
+      string_builder.append(String.format("│ %4dms ", rrhc_stats.average_cpu_time[d]));
+      string_builder.append(String.format("│ %4dms ", gen_stats.average_cpu_time[d]));
+      string_builder.append(String.format("│ %6.0f ", rrhc_stats.average_restarts[d]));
+      string_builder.append(String.format("│ %6.0f ", gen_stats.average_generations[d]));
+      string_builder.append(String.format("│ %3.0f%% ", rrhc_stats.percent_solved[d] * 100));
+      string_builder.append(String.format("│ %3.0f%% │\n", gen_stats.percent_solved[d] * 100));
     }
-    System.out.println("└────┴──────────┴──────────┴────────┴────────┴────────┴────────┴──────┴──────┘");
+    string_builder.append("└────┴──────────┴──────────┴────────┴────────┴────────┴────────┴──────┴──────┘\n");
+
+    return string_builder.toString();
   }
 
   private static Stats getGeneticSolution (int min, int max, int trials, int population_size, double mutation_probability) {
@@ -146,6 +156,18 @@ public class NQueen {
     }
 
     return stats;
+  }
+
+  private static String askFileName () {
+    Scanner scanner = new Scanner(System.in);
+    String n = "";
+    System.out.print("Enter output file name\n: ");
+    while (!scanner.hasNextLine()) scanner.next();
+    n = scanner.next();
+    if (n.length() == 0) {
+      return "out.txt";
+    }
+    return n;
   }
 
   private static int askTrials () {
@@ -208,7 +230,7 @@ public class NQueen {
       System.out.print("Enter the largest board size you want to solve:\n: ");
       while (!scanner.hasNextInt()) scanner.next();
       end = scanner.nextInt();
-      if (end > start) {
+      if (end >= start) {
         break;
       }
       System.out.println("Choose a number greater than or equal to the start size!");
