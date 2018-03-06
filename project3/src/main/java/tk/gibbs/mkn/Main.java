ee4b9f0a9b210499aa5475c6b16dda8afa8c2acb
Louis Orleans <louis@orleans.io>
2018-03-05 23:17:31 -0800

    💩 add test prints

--- project3/src/main/java/tk/gibbs/mkn/Main.java
+++ project3/src/main/java/tk/gibbs/mkn/Main.java
@@ -2,6 +2,8 @@ package tk.gibbs.mkn;
 
 public class Main {
   public static void main (String[] args) {
+    System.out.println("Hello, World");
 
+    System.out.println(new BoardState().toString());
   }
 }
