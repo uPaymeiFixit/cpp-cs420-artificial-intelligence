c123fcf9f22cc2d10b18644461203df99c05fcb5
Louis Orleans <louis@orleans.io>
2018-03-06 13:26:41 -0800

    🔨 make BoardState immutable

--- project3/src/main/java/tk/gibbs/mnk/BoardState.java
+++ project3/src/main/java/tk/gibbs/mnk/BoardState.java
@@ -1,35 +1,55 @@
 package tk.gibbs.mnk;
 
 public class BoardState {
-  public TileState[][] board = new TileState[8][8];
-  public String hash;
-  public int heuristicVal;
-  public int last_move_x;
-  public int last_move_y;
-  public int depth = 0;
+  final public TileState[][] board;
+  final public String hash;
+  final public int heuristicVal;
+  final public int last_move_x;
+  final public int last_move_y;
+  final public int depth;
+
+  // TileState[][] getBoard () { return board; }
+  // String getHash () { return hash; }
+  // int getHeuristicVal () { return heuristicVal; }
+  // int getLastMoveX () { return last_move_x; }
+  // int getLastMoveY () { return last_move_y; }
 
   BoardState () {
-    // Initilaize empty board
+    // Initilaize with empty board
+    this(generateEmptyBoard());
+  }
+
+  BoardState (TileState[][] board) {
+    this(board, 0, 0, 0);
+  }
+
+  BoardState (TileState[][] board, int last_move_x, int last_move_y, int depth) {
+    this.board = board;
+    this.last_move_x = 0;
+    this.last_move_y = 0;
+    this.depth = depth;
+    // TODO: calculate heuristicVal & hash
+    this.heuristicVal = 0;
+    this.hash = "hash";
+  }
+
+  static TileState[][] generateEmptyBoard () {
+    TileState[][] board = new TileState[8][8];
     for (int i = 0; i < board.length; i++) {
       for (int n = 0; n < board[i].length; n++) {
         board[i][n] = TileState.EMPTY;
       }
     }
-  }
-
-  BoardState (TileState[][] board) {
-    this.board = board;
-    // TODO: calculate heuristicVal & hash
+    return board;
   }
 
   BoardState copyBoardWithMove (TileState player, int col, int row) {
-    if (player == TileState.EMPTY) {
-      throw new Error("TileState.EMPTY is not a valid player");
-    } else if (col < 0 || col >= board.length) {
-      throw new Error(String.format("col %s is outside of board", col));
-    } else if (row < 0 || row >= board[col].length) {
-      throw new Error(String.format("row %s is outside of board", row));
-    }
+    assert player != TileState.EMPTY;
+    // } else if (col < 0 || col >= board.length) {
+    //   throw new Error(String.format("col %s is outside of board", col));
+    // } else if (row < 0 || row >= board[col].length) {
+    //   throw new Error(String.format("row %s is outside of board", row));
+    // }
 
     TileState[][] updatedBoard = new TileState[this.board.length][];
     for (int i = 0; i < this.board.length; i++) {
@@ -38,11 +58,7 @@ public class BoardState {
 
     updatedBoard[col][row] = player;
 
-    BoardState new_board = new BoardState(updatedBoard);
-    new_board.last_move_x = col;
-    new_board.last_move_y = row;
-    new_board.depth = this.depth + 1;
-    return new_board;
+    return new BoardState(updatedBoard, col, row, this.depth + 1);
   }
 
   @Override
