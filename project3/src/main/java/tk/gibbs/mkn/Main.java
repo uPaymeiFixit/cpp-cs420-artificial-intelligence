5b169ab22f56d95ad26ce58d0ac45c7197593628
Josh Gibbs <upaymeifixit@gmail.com>
2018-03-05 23:17:25 -0800

    Added menu item to ask for maximum turn time

--- project3/src/main/java/tk/gibbs/mkn/Main.java
+++ project3/src/main/java/tk/gibbs/mkn/Main.java
@@ -1,7 +1,30 @@
 package tk.gibbs.mkn;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main (String[] args) {
+    menu();
+  }
 
+  public static void menu () {
+    double max_time = askMaxTime();
+    System.out.println(max_time);
+  }
+
+  public static double askMaxTime () {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter maximum turn time in seconds\n: ");
+    while (!scanner.hasNextDouble()) {
+      scanner.next();
+    }
+    double max_time = scanner.nextDouble();
+    if (max_time <= 0) {
+      System.out.println("Invalid input: Time in seconds must be greater than 0.");
+      scanner.close();
+      return askMaxTime();
+    }
+    scanner.close();
+    return max_time;
   }
 }
