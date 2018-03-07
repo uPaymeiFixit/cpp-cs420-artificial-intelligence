65ee50a9024cee2974f961ab2cf601f2f449d558
Louis Orleans <louis@orleans.io>
2018-03-06 18:05:22 -0800

    ✨ add toString for Pattern

--- project3/src/main/java/tk/gibbs/mnk/Pattern.java
+++ project3/src/main/java/tk/gibbs/mnk/Pattern.java
@@ -1,5 +1,7 @@
 package tk.gibbs.mnk;
 
+import java.awt.Rectangle;
+
 public class Pattern {
   /**
    * List of points in pattern. Always starts at (0,0)
@@ -27,5 +29,55 @@ public class Pattern {
       }
       return rotate(--timesCW, rotated);
     }
+  }
+
+  @Override
+  public String toString () {
+    String out = "";
+
+    Rectangle boundingBox = new Rectangle(0, 0, 0, 0);
+
+    for (int i = 0; i < this.points.length; i++) {
+      if (!boundingBox.contains(this.points[i].x, this.points[i].y)) {
+        if (this.points[i].x > boundingBox.x + boundingBox.width) {
+          boundingBox.width = this.points[i].x - boundingBox.x;
+        }
+        if (this.points[i].x < boundingBox.x) {
+          boundingBox.x = this.points[i].x;
+        }
+        if (this.points[i].y > boundingBox.y + boundingBox.height) {
+          boundingBox.height = this.points[i].y - boundingBox.y;
+        }
+        if (this.points[i].y < boundingBox.y) {
+          boundingBox.y = this.points[i].y;
+        }
+      }
+    }
+
+    for (int i = 0; i <= boundingBox.height; i++) {
+      for (int j = 0; j <= boundingBox.width; j++) {
+        BoardTile tile = null;
+
+        for (BoardTile t : this.points) {
+          if (t.x == boundingBox.x + j && t.y == boundingBox.y + i) {
+            tile = t;
+            break;
+          }
+        }
+
+        if (tile == null) {
+          out += String.format("%s?%s ", ANSIColors.BLACK_BRIGHT, ANSIColors.RESET);
+        } else if (tile.state == TileState.EMPTY) {
+          out += String.format("%s∙%s ", ANSIColors.WHITE, ANSIColors.RESET);
+        } else if (tile.state == TileState.X) {
+          out += String.format("%s✕%s ", ANSIColors.GREEN, ANSIColors.RESET);
+        } else if (tile.state == TileState.O) {
+          out += String.format("%s◯%s ", ANSIColors.RED, ANSIColors.RESET);
+        }
+      }
+      out += "\n";
+    }
+
+    return out;
   }
 }
